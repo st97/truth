10e177fdba4a4ab15ac1f5f5dcf1daf3332e3dd1
st97 <joseph2717@yahoo.com>
2025-03-27 21:44:41 +0000

    Improve tests - handling edge cases

--- plugin/src/ui/hooks.ts
+++ plugin/src/ui/hooks.ts
@@ -1,7 +1,7 @@
 import { type TooltipOptions, setTooltip } from "obsidian";
 import { useEffect } from "react";
 
-export let useObsidianTooltip = (
+export const useObsidianTooltip = (
   ref: HTMLElement | null,
   text: string,
   options?: TooltipOptions,
