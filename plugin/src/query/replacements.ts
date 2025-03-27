196dddfde1e6dcaafabb54ba56ef198b482cb061
st97 <joseph2717@yahoo.com>
2025-03-27 23:29:35 +0000

    Remove styles - refactoring code

--- plugin/src/query/replacements.ts
+++ plugin/src/query/replacements.ts
@@ -3,6 +3,6 @@ import type { MarkdownPostProcessorContext } from "obsidian";
 
 export function applyReplacements(query: Query, ctx: MarkdownPostProcessorContext) {
   // Replace {filename} with the base file name of file where the query originated.
-  const baseFileName = ctx.sourcePath.replace(/.*\//, "").replace(/\.md$/i, "");
+  let baseFileName = ctx.sourcePath.replace(/.*\//, "").replace(/\.md$/i, "");
   query.filter = query.filter.replace(/{{filename}}/g, baseFileName);
 }
